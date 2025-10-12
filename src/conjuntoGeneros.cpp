f42548c3c0ffd1a14a61f3acda19b2f1f8bc1247
Luxy <luciacosta200626@gmail.com>
2025-10-12 19:07:10 -0300

    Update conjuntoGeneros.cpp

--- src/conjuntoGeneros.cpp
+++ src/conjuntoGeneros.cpp
@@ -1,53 +1,75 @@
 #include "../include/conjuntoGeneros.h"
 
-struct rep_conjuntogeneros{
-
+struct rep_conjuntogeneros
+{
+  int *ids;
+  int cantActual;
+  int cantMax;
 };
 
-TConjuntoGeneros crearTConjuntoGeneros(int cantMax){
-  return NULL;    
+TConjuntoGeneros crearTConjuntoGeneros(int cantMax)
+{
+  TConjuntoGeneros nuevo = new rep_conjuntogeneros;
+  nuevo->ids = new int[cantMax];
+  nuevo->cantActual = 0;
+  nuevo->cantMax = cantMax;
+  return nuevo;
 }
 
-bool esVacioTConjuntoGeneros(TConjuntoGeneros c){
+bool esVacioTConjuntoGeneros(TConjuntoGeneros c)
+{
+  if (c == NULL)
+  {
+    return true;
+  }
+  return false;
+}
+
+void insertarTConjuntoGeneros(TConjuntoGeneros &c, int id)
+{
+}
+
+void borrarDeTConjuntoGeneros(TConjuntoGeneros &c, int id)
+{
+}
+
+bool perteneceTConjuntoGeneros(TConjuntoGeneros c, int id)
+{
   return true;
 }
 
-void insertarTConjuntoGeneros(TConjuntoGeneros &c, int id){
-
-}
-
-void borrarDeTConjuntoGeneros(TConjuntoGeneros &c, int id){
-
-}
-
-bool perteneceTConjuntoGeneros(TConjuntoGeneros c, int id){
-  return true;
-}
-
-int cardinalTConjuntoGeneros(TConjuntoGeneros c){
+int cardinalTConjuntoGeneros(TConjuntoGeneros c)
+{
   return 0;
 }
 
-int cantMaxTConjuntoGeneros(TConjuntoGeneros c){
+int cantMaxTConjuntoGeneros(TConjuntoGeneros c)
+{
   return 0;
 }
 
-void imprimirTConjuntoGeneros(TConjuntoGeneros c){
-
+void imprimirTConjuntoGeneros(TConjuntoGeneros c)
+{
 }
 
-void liberarTConjuntoGeneros(TConjuntoGeneros &c){
-
+void liberarTConjuntoGeneros(TConjuntoGeneros &c)
+{
+  delete[] c->ids;
+  delete c;
+  c = NULL;
 }
 
-TConjuntoGeneros unionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
+TConjuntoGeneros unionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2)
+{
   return NULL;
 }
 
-TConjuntoGeneros interseccionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
+TConjuntoGeneros interseccionTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2)
+{
   return NULL;
 }
 
-TConjuntoGeneros diferenciaTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2){
+TConjuntoGeneros diferenciaTConjuntoGeneros(TConjuntoGeneros c1, TConjuntoGeneros c2)
+{
   return NULL;
 }
