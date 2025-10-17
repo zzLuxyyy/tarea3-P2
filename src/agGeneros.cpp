f38dfd3a7a5d3af13cc5562bfadc24a8d2e76be2
Luxy <luciacosta200626@gmail.com>
2025-10-16 22:00:22 -0300

    lucia

--- src/agGeneros.cpp
+++ src/agGeneros.cpp
@@ -1,51 +1,79 @@
-
 #include "../include/agGeneros.h"
 #include <assert.h>
 
-
-struct rep_agGeneros {
+struct rep_agGeneros
+{
+  int idGenero;
+  rep_agGeneros *pH;
+  rep_agGeneros *sH;
+  char nombreGenero[MAX_NOMBRE];
 };
 
-TAGGeneros crearTAGGeneros(){
-  return NULL;
+TAGGeneros crearTAGGeneros()
+{
+  TAGGeneros nuevo = new rep_agGeneros;
+  nuevo->idGenero = 0;
+  nuevo->pH = NULL;
+  nuevo->sH = NULL;
+  nuevo->nombreGenero[MAX_NOMBRE - 1] = '\0';
+
+  return nuevo;
 }
 
-void insertarGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGeneroPadre, int idGenero, const char nombreGenero[MAX_NOMBRE] ){
+void insertarGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGeneroPadre, int idGenero, const char nombreGenero[MAX_NOMBRE])
+{
 }
 
-void imprimirTAGGeneros(TAGGeneros arbolGeneros){
+void imprimirTAGGeneros(TAGGeneros arbolGeneros)
+{
 }
 
-void liberarTAGGeneros(TAGGeneros &arbolGeneros){
+void liberarTAGGeneros(TAGGeneros &arbolGeneros)
+{
+  if (arbolGeneros != NULL)
+  {
+    liberarTAGGeneros(arbolGeneros->pH);
+    liberarTAGGeneros(arbolGeneros->sH);
+    delete arbolGeneros;
+    arbolGeneros = NULL;
+  }
 }
 
-bool existeGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero){
+bool existeGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return false;
 }
 
-char* nombreGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero){
+char *nombreGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-nat alturaTAGGeneros(TAGGeneros arbolGeneros){
+nat alturaTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-nat cantidadTAGGeneros(TAGGeneros arbolGeneros){
+nat cantidadTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-TAGGeneros obtenerSubarbolTAGGeneros(TAGGeneros arbolGeneros, int idGenero) {
+TAGGeneros obtenerSubarbolTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-TConjuntoGeneros obtenerConjuntoGeneros(TAGGeneros arbolGeneros, int idGenero) {
+TConjuntoGeneros obtenerConjuntoGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-int obtenerMaxTAGGeneros(TAGGeneros arbolGeneros) {
+int obtenerMaxTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-void removerGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGenero){
+void removerGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGenero)
+{
 }
--- src/conjuntoGeneros.cpp
+++ src/conjuntoGeneros.cpp
@@ -3,30 +3,40 @@
 struct rep_conjuntogeneros
 {
   int *ids;
-  int cantActual;
-  int cantMax;
+  int cardinal;
+  int capacidad;
 };
 
 TConjuntoGeneros crearTConjuntoGeneros(int cantMax)
 {
   TConjuntoGeneros nuevo = new rep_conjuntogeneros;
   nuevo->ids = new int[cantMax];
-  nuevo->cantActual = 0;
-  nuevo->cantMax = cantMax;
+
+  for (int i = 0; i < cantMax; i++)
+  {
+    nuevo->ids[i] = 0;
+  }
+
+  nuevo->cardinal = 0;
+  nuevo->capacidad = cantMax;
   return nuevo;
 }
 
 bool esVacioTConjuntoGeneros(TConjuntoGeneros c)
 {
-  if (c == NULL)
-  {
-    return true;
-  }
-  return false;
+  return c->cardinal == 0;
 }
 
 void insertarTConjuntoGeneros(TConjuntoGeneros &c, int id)
 {
+  if (id >= 0 && id < c->capacidad)
+  {
+    if (c->ids[id] == 0)
+    {
+      c->ids[id] = 1;
+      c->cardinal++;
+    }
+  }
 }
 
 void borrarDeTConjuntoGeneros(TConjuntoGeneros &c, int id)
@@ -40,16 +50,31 @@ bool perteneceTConjuntoGeneros(TConjuntoGeneros c, int id)
 
 int cardinalTConjuntoGeneros(TConjuntoGeneros c)
 {
-  return 0;
+  return c->cardinal;
 }
 
 int cantMaxTConjuntoGeneros(TConjuntoGeneros c)
 {
-  return 0;
+  return c->capacidad;
 }
 
 void imprimirTConjuntoGeneros(TConjuntoGeneros c)
 {
+  bool primer_elemento = true; // Flag para el primer elemento
+
+  for (int i = 0; i < c->capacidad; i++) // Respeta el O(n) siendo n la cantMax
+  {
+    if (c->ids[i] == 1) // Si el id pertenece al conjunto
+    {
+      if (!primer_elemento) // Si no es el primer elemento
+      {                     // Imprime espacio antes del elemento
+        printf(" ");
+      }
+      printf("%d", i); // Imprime el id
+      primer_elemento = false;
+    }
+  }
+  printf("\n");
 }
 
 void liberarTConjuntoGeneros(TConjuntoGeneros &c)
